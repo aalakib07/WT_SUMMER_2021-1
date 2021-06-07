1384245792aafd658cc455ac13d943e195156531
Shahed1998 <shahedchowdhury120@gmail.com>
2021-06-07 16:04:23 +0600

    class task => post-example

---
+++ Task-1/practice.php
@@ -0,0 +1,6 @@
+<?php 
+    $cars = array('Volvo','BMW','Toyota');
+    var_dump($cars);
+    echo('<br/>');
+    print_r($cars);
+?>
---
+++ post-example/post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index page</title>
+</head>
+<body>
+    <form action="/Web-tech/post-example/controls/myAction.php" method="POST">
+        <label for="UserName">User Name</label><br>
+        <input type="text" name="uName" id="UserName"><br>
+        <label for="Email">Email</label><br>
+        <input type="email" name="eMail" id="Email"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
