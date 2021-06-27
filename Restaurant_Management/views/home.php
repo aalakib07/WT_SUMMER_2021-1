f5908e6cb9a12dcba13c13be1d7ffa577ec61a3f
Shahed1998 <shahedchowdhury120@gmail.com>
2021-06-28 02:00:35 +0600

    Template

---
+++ Restaurant_Management/views/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Management</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Admin Page</td>
+            <td><a href="admin/index.php">Enter</a></td>
+        </tr>
+        <tr>
+            <td>Customer Page</td>
+            <td><a href="customer/index.php">Enter</a></td>
+        </tr>
+        <tr>
+            <td>Stuff Page</td>
+            <td><a href="stuff/index.php">Enter</a></td>
+        </tr>
+        <tr>
+            <td>Supplier Page</td>
+            <td><a href="supplier/index.php">Enter</a></td>
+        </tr>
+    </table>
+</body>
+</html>
